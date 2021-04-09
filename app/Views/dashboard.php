e8d2f2389f514e69cebf2b2176c5abc0ece88b05
Nela Achillesová <Nelaach@seznam.cz>
2021-04-09 13:31:21 +0200

    menší změna

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,8 +1,26 @@
+
+<HTML>
+<head> 
+<style>img{
+text-align:center;
+}
+.centered {
+text-align: center;
+
+} 
+
+</style>
+
+
+</head>
+<body>
 <div class="container">
   <div class="row">
     <div class="col-12">
-      <h1>Hello, <?= session()->get('firstname') ?></h1>
+      <h1 class="centered">Vítej, <?= session()->get('firstname') ?></h1>
       
     </div>
   </div>
 </div>
+</body>
+</html>
